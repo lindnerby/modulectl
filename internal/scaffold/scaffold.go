f178e38f8b7a96b575ab7bfbd68d1cf44ede672a
Christoph Schwägerl <c.schwaegerl@sap.com>
2024-04-30 11:49:41 +0200

    comment TODOs for Adi

--- internal/scaffold/scaffold.go
+++ internal/scaffold/scaffold.go
@@ -25,6 +25,27 @@ type FileGeneratorService interface {
 	GenerateFile(out io.Out, path string, args types.KeyValueArgs) error
 }
 
+// TODO @Adi: Refactor so that 'manifestService' and 'defaultCRService' to use the FileGeneratorService interface and `ReuseFileGeneratorService` implementation
+// 'securityConfigService' provides an overall exmaple
+// please collect all questions you got while working on this task
+// please create a separate branch and open a PR targetting the current branch
+// we will have a look at the PR and discuss your questions together next week
+// outline:
+//   - change 'manifestService' and 'defaultCRService' to be of type `FileGeneratorService`
+//     adapt the code in the CreateScaffold method accordingly (i.e., use the new method signature)
+//   - create new contententproviders for manifest and defaultCR
+//     see contentprovider/securityconfig.go as an example
+//     in this case, we don't need the yaml converter and args (just pass nil or an empty map)
+//     the default content (e.g., from manifest.go#getDefaultContent) should be returned from the new content providers
+//     include a simple test that assures that the returned string is the expected default content (similar to how we test flag defaults)
+//   - update the composition root in cmd/modulectl/create/scaffold/scaffold.go
+//     instead of 'NewManifestService' and 'NewDefaultCRService' a new 'NewReuseFileGeneratorService' should be used for each of those using the respective content provider
+//     see the security config part as an example (remember, yaml converter is not needed for the new ones)
+//   - refactor the related tests in 'scaffold_test'
+//     the stubs for manifestService and defaultCRService can be replaced with stubs for FileGenerator
+//     again, see security config as an example
+//   - remove the 'ManifestService' and 'DefaultCRService' interfaces from this file
+//   - remove the 'ManifestService' and 'DefaultCRService' implementations from internal/scaffold/manifest/manifest.go and internal/scaffold/defaultcr/defaultcr.go
 type ScaffoldService struct {
 	moduleConfigService   ModuleConfigService
 	manifestService       ManifestService
@@ -53,6 +74,11 @@ func (s *ScaffoldService) CreateScaffold(opts Options) error {
 		return err
 	}
 
+	// TODO @Adi: We use 'path.Join(opts.Directory, ...)' to create the file paths at various places here
+	// it should work regardless the user provides a relative or absolute path
+	// as of now, I think it only works with absolute paths for 'opts.Directory'
+	// please verify if this observation is true and if so, please fix it
+	// do some research on how to handle file paths in Go properly and update the code accordingly
 	manifestFilePath := path.Join(opts.Directory, opts.ManifestFileName)
 	if err := s.manifestService.GenerateManifestFile(opts.Out, manifestFilePath); err != nil {
 		return err
