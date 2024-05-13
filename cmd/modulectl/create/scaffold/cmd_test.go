486573e95393a41e4e4b1af767f69c1840a5c1d1
Christoph Schwägerl <c.schwaegerl@sap.com>
2024-05-13 14:39:54 +0200

    unit test flag parsing

--- cmd/modulectl/create/scaffold/cmd_test.go
+++ cmd/modulectl/create/scaffold/cmd_test.go
@@ -1,6 +1,9 @@
 package scaffold_test
 
 import (
+	"fmt"
+	"math/rand"
+	"os"
 	"testing"
 
 	scaffoldcmd "github.com/kyma-project/modulectl/cmd/modulectl/create/scaffold"
@@ -17,15 +20,13 @@ func Test_NewCmd_ReturnsError_WhenScaffoldServiceIsNil(t *testing.T) {
 }
 
 func Test_NewCmd_Succceeds(t *testing.T) {
-	svc := &scaffoldServiceStub{called: false}
-	_, err := scaffoldcmd.NewCmd(svc)
+	_, err := scaffoldcmd.NewCmd(&scaffoldServiceStub{})
 
 	require.NoError(t, err)
-	require.False(t, svc.called)
 }
 
 func Test_Execute_CallsScaffoldService(t *testing.T) {
-	svc := &scaffoldServiceStub{called: false}
+	svc := &scaffoldServiceStub{}
 	cmd, _ := scaffoldcmd.NewCmd(svc)
 
 	err := cmd.Execute()
@@ -34,15 +35,119 @@ func Test_Execute_CallsScaffoldService(t *testing.T) {
 	require.True(t, svc.called)
 }
 
+func Test_Execute_ParsesOptions(t *testing.T) {
+	directory := getRandomName(10)
+	moduleConfigFile := getRandomName(10)
+	manifestFile := getRandomName(10)
+	defaultCRFile := getRandomName(10)
+	securityConfigFile := getRandomName(10)
+	moduleName := getRandomName(10)
+	moduleVersion := "1.1.1"
+	moduleChannel := getRandomName(10)
+	os.Args = []string{
+		"scaffold",
+		"--directory", directory,
+		"--module-config", moduleConfigFile,
+		"--overwrite",
+		"--gen-manifest", manifestFile,
+		fmt.Sprintf("--gen-default-cr=%s", defaultCRFile),
+		fmt.Sprintf("--gen-security-config=%s", securityConfigFile),
+		"--module-name", moduleName,
+		"--module-version", moduleVersion,
+		"--module-channel", moduleChannel,
+	}
+	svc := &scaffoldServiceStub{}
+	cmd, _ := scaffoldcmd.NewCmd(svc)
+
+	cmd.Execute()
+
+	assert.Equal(t, moduleName, svc.opts.ModuleName)
+	assert.Equal(t, directory, svc.opts.Directory)
+	assert.Equal(t, moduleConfigFile, svc.opts.ModuleConfigFileName)
+	assert.Equal(t, true, svc.opts.ModuleConfigFileOverwrite)
+	assert.Equal(t, manifestFile, svc.opts.ManifestFileName)
+	assert.Equal(t, defaultCRFile, svc.opts.DefaultCRFileName)
+	assert.Equal(t, securityConfigFile, svc.opts.SecurityConfigFileName)
+	assert.Equal(t, moduleVersion, svc.opts.ModuleVersion)
+	assert.Equal(t, moduleChannel, svc.opts.ModuleChannel)
+}
+
+func Test_Execute_ParsesShortOptions(t *testing.T) {
+	directory := getRandomName(10)
+	os.Args = []string{
+		"scaffold",
+		"-d", directory,
+		"-o",
+	}
+	svc := &scaffoldServiceStub{}
+	cmd, _ := scaffoldcmd.NewCmd(svc)
+
+	cmd.Execute()
+
+	assert.Equal(t, directory, svc.opts.Directory)
+	assert.Equal(t, true, svc.opts.ModuleConfigFileOverwrite)
+}
+
+func Test_Execute_ParsesDefaults(t *testing.T) {
+	os.Args = []string{
+		"scaffold",
+	}
+	svc := &scaffoldServiceStub{}
+	cmd, _ := scaffoldcmd.NewCmd(svc)
+
+	cmd.Execute()
+
+	assert.Equal(t, scaffoldcmd.ModuleNameFlagDefault, svc.opts.ModuleName)
+	assert.Equal(t, scaffoldcmd.DirectoryFlagDefault, svc.opts.Directory)
+	assert.Equal(t, scaffoldcmd.ModuleConfigFileFlagDefault, svc.opts.ModuleConfigFileName)
+	assert.Equal(t, scaffoldcmd.ModuleConfigFileOverwriteFlagDefault, svc.opts.ModuleConfigFileOverwrite)
+	assert.Equal(t, scaffoldcmd.ManifestFileFlagDefault, svc.opts.ManifestFileName)
+	assert.Equal(t, scaffoldcmd.DefaultCRFlagDefault, svc.opts.DefaultCRFileName)
+	assert.Equal(t, scaffoldcmd.SecurityConfigFileFlagDefault, svc.opts.SecurityConfigFileName)
+	assert.Equal(t, scaffoldcmd.ModuleVersionFlagDefault, svc.opts.ModuleVersion)
+	assert.Equal(t, scaffoldcmd.ModuleChannelFlagDefault, svc.opts.ModuleChannel)
+}
+
+func Test_Execute_ParsesNoOptDefaults(t *testing.T) {
+	os.Args = []string{
+		"scaffold",
+		"--gen-default-cr",
+		"--gen-security-config",
+	}
+	svc := &scaffoldServiceStub{}
+	cmd, _ := scaffoldcmd.NewCmd(svc)
+
+	cmd.Execute()
+
+	assert.Equal(t, scaffoldcmd.DefaultCRFlagNoOptDefault, svc.opts.DefaultCRFileName)
+	assert.Equal(t, scaffoldcmd.SecurityConfigFileFlagNoOptDefault, svc.opts.SecurityConfigFileName)
+}
+
 // ***************
 // Test Stubs
 // ***************
 
 type scaffoldServiceStub struct {
 	called bool
+	opts   scaffoldsvc.Options
 }
 
-func (s *scaffoldServiceStub) CreateScaffold(_ scaffoldsvc.Options) error {
+func (s *scaffoldServiceStub) CreateScaffold(opts scaffoldsvc.Options) error {
 	s.called = true
+	s.opts = opts
 	return nil
+}
+
+// ***************
+// Test Helpers
+// ***************
+
+const charset = "abcdefghijklmnopqrstuvwxyz"
+
+func getRandomName(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
 }
