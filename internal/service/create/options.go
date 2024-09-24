08d649194befe46fffd34f5b891c71d15ec5a831
Badr, Nesma <nesma.badr@sap.com>
2024-09-24 12:38:34 +0200

    Revert "Add registryUrl != "" check"
    This reverts commit f26ab1ee6f8065547c6b26a4519a4baf7d290082.

--- internal/service/create/options.go
+++ internal/service/create/options.go
@@ -45,7 +45,7 @@ func (opts Options) Validate() error {
 		return fmt.Errorf("%w:  opts.TemplateOutput must not be empty", commonerrors.ErrInvalidOption)
 	}
 
-	if opts.RegistryURL != "" && !strings.HasPrefix(opts.RegistryURL, "http") {
+	if !strings.HasPrefix(opts.RegistryURL, "http") {
 		return fmt.Errorf("%w:  opts.RegistryURL does not start with http(s)", commonerrors.ErrInvalidOption)
 	}
 
